095edb6713b5d4366471f6ab4395cab11b1d88af
Andrew Palmer <andrew.palmer.10@gmail.com>
2017-12-26 11:25:30 -0500

    Add new plugin with env vars

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  foo: process.env.FOO || 'webpack whaddup!',
+};
+
+config.globals = {
+  'process.env': {
+    NODE_ENV: JSON.stringify(config.env),
+  },
+  NODE_ENV: config.env,
+};
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -26,28 +39,29 @@ module.exports = {
           use: [
             { loader: 'css-loader',
               query: {
-                minimize: true
-              }
+                minimize: true,
+              },
             },
             { loader: 'postcss-loader' },
             { loader: 'sass-loader' },
-            { loader: 'import-glob-loader' }
-          ]
-        })
-      }
+            { loader: 'import-glob-loader' },
+          ],
+        }),
+      },
     ],
   },
   plugins: [
     new ExtractTextPlugin({
       allChunks: true,
-      filename: "bundle.css"
-    })
+      filename: 'bundle.css',
+    }),
+    new webpack.DefinePlugin(config.globals),
   ],
   resolve: {
     modules: [
       path.join(__dirname, 'node_modules'),
       path.join(__dirname, 'src'),
     ],
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
 };
