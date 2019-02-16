2f024cea8d8caaf536b5fd3cff03cfcf57796b1e
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-02-16 15:21:09 -0500

    You saved your fake data, dummy

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -8,44 +8,7 @@ const WHITE = '#ffffff';
 
 class Chart extends Component {
   render () {
-    // const { data } = this.props;
-    const data = [
-      {
-        precip: 10,
-        temp: 30,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 40,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 10,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 10,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 10,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 10,
-        time: 'foo',
-      },
-      {
-        precip: 10,
-        temp: 30,
-        time: 'foo',
-      },
-    ];
+    const { data } = this.props;
     const whiteBars = { fill: WHITE };
 
     if (!data) return false;
